3b4e8d2d2db4db9180b1e2e4e6dd34020bad9d91
Alex <pavezzalex@gmail.com>
2024-04-26 18:29:28 -0400

    avances

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    	path('', index, name="index")
+    	path('', index, name="index"),
+        path('shop.html', productos, name= "productos"),
+        path('cart.html', carrito, name = "carrito"),
+        path('detail.html', detalleProducto, name="detalleProducto")
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'core/index.html')
+
+def productos(request):
+    return render(request, 'core/shop.html')
+
+def carrito(request):
+    return render(request, 'core/cart.html')
+
+def detalleProducto(request):
+    return render (request, 'core/detail.html')
