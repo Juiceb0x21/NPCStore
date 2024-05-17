ad8fb4f5b9dfab1bec511d5c9cce240d86749546
Alan Moreno <alanmoreno332@gmail.com>
2024-05-17 00:01:17 -0400

    fix coma

--- NPCstore/settings.py
+++ NPCstore/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'core.context_processor.user_data'
+                'core.context_processor.user_data',
                 'carro.context_processor.importe_total_carro',
                 'carro.context_processor.cantidad_carro',
             ],
