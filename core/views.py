5d93b4779010f08804c55a95a716ec8ddd4d4ecd
Qlacreo <ale.oliva@duocuc.cl>
2024-05-16 23:28:57 -0400

    Avance productos

--- core/views.py
+++ core/views.py
@@ -41,8 +41,33 @@ def login(request):
 
     return render(request, 'core/login.html', {'form': form})
 
+def obtener_productos():
+    try:
+        response = requests.get('http://127.0.0.1:5000/api/productos')
+        response.raise_for_status()  # Lanzar una excepción para errores de solicitud
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        
+        print(f'Error al obtener los productos: {str(e)}')
+        return None
+
 def productos(request):
-    return render(request, 'core/shop.html')
+    productos = obtener_productos()
+    categoria = request.GET.get('categoria', '')
+    marca = request.GET.get('marca', '')
+    busqueda = request.GET.get('busqueda', '')
+
+    if productos:
+        if categoria:
+            productos = [p for p in productos if p['categoria'] == categoria]
+        if marca:
+            productos = [p for p in productos if p['marca'] == marca]
+        if busqueda:
+            productos = [p for p in productos if busqueda.lower() in p['nombre'].lower()]
+
+        return render(request, 'core/shop.html', {'productos': productos})
+    else:
+        return render(request, 'core/shop.html', {'error': 'No se pudo obtener los productos'})
 
 def carrito(request):
     return render(request, 'core/cart.html')
